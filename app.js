ef24463c3c5e2b3ee6ecc54b2bcdd1d35ad91c7b
Justin Murrah <jdmurrah@gmail.com>
2022-11-04 11:02:28 -0500

    submit button adds h2 with text value

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 $(document).ready(function () {
-    
 
     $('body').append('<div></div>')
 
-
+    
+    
     // reference to submit button click event added
-    $('#btnSubmit').click(function () {
-    
-    //sends and alert message with the text in the string
-    alert("Don't Touch Me right THERE!");
-    
-    // alerts the value in the text input box
-    alert($('#textbox').val())
-    return;
-});
-
-$('#textbox').keyup(function () {
-    
-    let blank = false;
-    if ($('#textbox').val().length > 0) {
-        $('#btnSubmit').prop('disabled', false);
+    $('#btnSubmit').click(function (e) {
+         e.preventDefault();
+        //sends and alert message with the text in the string
+        alert("Don't Touch Me right THERE!");
         
-    };
-    
-    
-});
+        // alerts the value in the text input box
+        alert($('#textbox').val());
+        
+        
+        $('div').append('<h2>' + $('input').val() + '</h2>');
+         
+        
+    });
+
+    $('#textbox').keyup(function () {
+
+       
+        if ($('#textbox').val().length > 0) {
+            $('#btnSubmit').prop('disabled', false);
+            //return;
+        };
+
+
+    });
 });
 //$('#textbox').on('input change', function() {
     //       if($('#textbox').val().length ==  0) {
